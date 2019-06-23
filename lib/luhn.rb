3e469841d0390140028e7cd53f4f36495980cf83
Hugo Mejia <superboss74@protonmail.com>
2019-06-23 23:41:46 +0000

    Adding OOP principles to luhn.rb

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -1,14 +1,44 @@
 # The Luhn algorithm - https://en.wikipedia.org/wiki/Luhn_algorithm
 module Luhn
-  def self.is_valid?(number)
-    number = number.to_s.chars.map(&:to_i).reverse
-    number.map!.with_index do |num, index|
-      if index.odd?
-        num *= 2
-        num -= 9 if num >= 10
+  def self.card_to_digits(card_number)
+    card_number.to_s.chars.map(&:to_i)
+  end
+
+  def self.reverse_digits(list_of_integers)
+    list_of_integers.reverse
+  end
+
+  def self.greater_than_10(number)
+    number *= 2
+    number -= 9 if number >= 10
+  end
+
+  def self.divisable_by_10?(list)
+    true ? (list.sum % 10).zero? : false
+  end
+
+  def self.subract_from_odd(number_list)
+    number_list.map!.with_index do |number, index|
+      # Luhn.greater_than_10(number, index)
+      # puts "current index is #{index} - current number is #{number}"
+      if index % 2 != 0
+        number *= 2
+        number -= 9 if number >= 10
+        puts "current number is #{number}"
+        puts "--------"
+      else
+        number
       end
-      num
+      number
     end
-    return true ? number.sum % 10 == 0 : false
+  end
+
+  def self.is_valid?(credit_card_number)
+    card_digits = Luhn.card_to_digits(credit_card_number)
+    digits_list = Luhn.reverse_digits(card_digits)
+    Luhn.subract_from_odd(digits_list)
+    Luhn.divisable_by_10?(digits_list)
   end
 end
+
+puts Luhn.is_valid?(4194560385008504)
