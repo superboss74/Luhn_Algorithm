55ee0809ce3eda651dd98007088933b591d515dc
Hugo Mejia <superboss74@protonmail.com>
2019-06-27 14:35:59 +0000

    refactoring two methods

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -8,10 +8,8 @@ module Luhn
     number_list.reverse
   end
 
-  def self.when_greater_10(number_list)
-    number_list = number_list.map do |number|
-      number >= 10 ? number - 9 : number
-    end
+  def self.when_above_nine(number_list)
+    number_list.map { |num| num >= 10 ? num - 9 : num }
   end
 
   def self.divisable_by_10?(number_list)
@@ -19,18 +17,14 @@ module Luhn
   end
 
   def self.every_second_digit(number_list)
-    number_list = number_list.map.with_index do |number, index|
-      next number if index.zero?
-
-      index.odd? ? number * 2 : number
-    end
+    number_list.map.with_index(1) { |num, index| index.even? ? num * 2 : num }
   end
 
   def self.is_valid?(credit_card_number)
     card_digits = Luhn.card_to_digits(credit_card_number)
     number_list = Luhn.reverse_digits(card_digits)
     number_list = Luhn.every_second_digit(number_list)
-    number_list = Luhn.when_greater_10(number_list)
+    number_list = Luhn.when_above_nine(number_list)
     Luhn.divisable_by_10?(number_list)
   end
 end
